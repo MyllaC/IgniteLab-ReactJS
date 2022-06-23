fb48a1f568c6d6e90224879e31771c929dc73716
Camila Cristina <criscamila09@gmail.com>
2022-06-23 15:11:57 -0300

    add player com o vimejs

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,4 @@
+import { DefaultUi, Player, Youtube } from '@vime/react'
 import {
   DiscordLogo,
   Lightning,
@@ -5,11 +6,18 @@ import {
   CaretRight
 } from 'phosphor-react'
 
+import '@vime/core/themes/default.css'
+
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="cUT665tW4v8" />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
